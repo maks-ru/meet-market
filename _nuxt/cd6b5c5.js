2efa0a547c0248c6161676baa08583d456f7d803
Maksim Merzlyakov <m_maxsim@mil.ru>
2022-02-16 23:17:32 +0500

    first commit

---
+++ _nuxt/cd6b5c5.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{359:function(e,n,t){"use strict";t.r(n);var l={name:"IndexPage"},c=t(15),component=Object(c.a)(l,(function(){var e=this.$createElement;return(this._self._c||e)("div")}),[],!1,null,null,null);n.default=component.exports}}]);
